5e725bde65fd5dfae0f18782063bd270b9ad52c2
ThomasDeJesus <tdejesus@stadetoulousain.fr>
2025-02-20 12:59:54 +0100

    test

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    API_URL : 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/StadeToulousainProd/SI%20-%20General/00.%20Pipelines/CREATE%20Qr%20Code%20Task',
+    BEARER_TOKEN : 'Hub0ZvaOvcT8Ztw4cTiRE7JsN1PXk4Wa'
+};
